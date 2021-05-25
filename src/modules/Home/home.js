66865615d15d4c11f9d47d85466893a666d76430
AbhiS66 <Abhi668412@gmail.com>
2021-05-25 17:16:48 +0530

    wip

---
+++ src/modules/Home/home.js
@@ -0,0 +1,6 @@
+import React, { useEffect } from 'react';
+import swal from "sweetalert";
+import { Col, Row } from "react-bootstrap";
+import { useLocation } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+
