2dc796665953593c24222b8f65d4265e34b711e1
AbhiS66 <Abhi668412@gmail.com>
2021-05-27 13:11:17 +0530

    wip

--- src/modules/Home/steps/lead-page/lead-page.js
+++ src/modules/Home/steps/lead-page/lead-page.js
@@ -1,14 +1,48 @@
 import React from "react";
 import { Row, Col, Form } from "react-bootstrap";
-import { Textbox, Button, ErrorMsg, Label, Error } from "components";
+import { Textbox, Button, Error, Label } from "components";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import * as yup from "yup";
-import { numOnly } from 'utils';
+import { numOnly } from "utils";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+// validation schema
+const yupValidate = yup.object({
+	email: yup
+		.string()
+		.email("Please enter valid email id")
+		.min(2, "Minimum 2 chars required")
+		.max(50, "Must be less than 50 chars")
+		.required("Email id is required"),
+	mobile_no: yup
+		.string()
+		.required("Mobile No. is required")
+		.matches(/^[7-9][0-9]{9}$/, "Must be only digits")
+		.min(10, "Mobile No. should be 10 digits")
+		.max(10, "Mobile No. should be 10 digits"),
+	last_name: yup
+		.string()
+		.required("Last Name is required")
+		.min(2, "Minimum 2 chars required")
+		.max(50, "Must be less than 50 chars")
+		.matches(/^([A-Za-z\s])+$/, "Must contain only alphabets"),
+	first_name: yup
+		.string()
+		.required("First Name is required")
+		.matches(/^([A-Za-z\s])+$/, "Must contain only alphabets")
+		.min(2, "Minimum 2 chars required")
+		.max(50, "Must be less than 50 chars"),
+});
 
 export const LeadPage = () => {
-	const { handleSubmit, register, errors } = useForm();
+	const { handleSubmit, register, errors } = useForm({
+		resolver: yupResolver(yupValidate),
+		mode: "all",
+		reValidateMode: "onBlur",
+	});
 	const handleChange = () => {};
+
 	const onSubmit = (data) => {
 		console.log(data);
 	};
@@ -35,7 +69,7 @@ export const LeadPage = () => {
 								onChange={handleChange}
 								error={errors?.first_name}
 							/>
-							{!!errors.first_name && <ErrorMsg>{errors.first_name.message}</ErrorMsg>}
+							{!!errors.first_name && <Error style={{marginTop:'-20px'}}>{errors.first_name.message}</Error>}
 						</div>
 					</Col>
 					<Col sm="12" md="6" lg="6" xl="6">
@@ -51,7 +85,7 @@ export const LeadPage = () => {
 								onChange={handleChange}
 								error={errors?.last_name}
 							/>
-							{!!errors.last_name && <ErrorMsg>{errors.last_name.message}</ErrorMsg>}
+							{!!errors.last_name && <Error style={{marginTop:'-20px'}}>{errors.last_name.message}</Error>}
 						</div>
 					</Col>
 					<Col sm="12" md="12" lg="12" xl="12">
@@ -67,10 +101,9 @@ export const LeadPage = () => {
 								onChange={handleChange}
 								error={errors?.mobile_no}
 								maxLength="10"
-                                onKeyDown={numOnly}
-
+								onKeyDown={numOnly}
 							/>
-							{!!errors.mobile_no && <ErrorMsg>{errors.mobile_no.message}</ErrorMsg>}
+							{!!errors.mobile_no && <Error style={{marginTop:'-20px'}}>{errors.mobile_no.message}</Error>}
 						</div>
 					</Col>
 					<Col sm="12" md="12" lg="12" xl="12">
@@ -86,8 +119,27 @@ export const LeadPage = () => {
 								onChange={handleChange}
 								error={errors?.email}
 							/>
-							{!!errors.email && <ErrorMsg>{errors.email.message}</ErrorMsg>}
+							{!!errors.email && <Error style={{marginTop:'-20px'}}>{errors.email.message}</Error>}
 						</div>
+					</Col>
+					<Col
+						sm="12"
+						md="12"
+						lg="12"
+						xl="12"
+						className="d-flex justify-content-center mt-3"
+					>
+						<Button
+							className=""
+							buttonStyle="outline-solid"
+							hex1={"#bdd400"}
+							hex2={"#bdd400"}
+							borderRadius={"20px"}
+							shadow={"none"}
+							type="submit"
+						>
+							Proceed
+						</Button>
 					</Col>
 				</Row>
 			</Form>
