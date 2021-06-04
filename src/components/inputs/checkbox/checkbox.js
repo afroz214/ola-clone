22dab7f59b818e0bb9704a4a0b17344f84918a29
soorajck <sooraj.kumar@fyntune.com>
2021-06-04 15:23:27 +0530

    addonApi

--- src/components/inputs/checkbox/checkbox.js
+++ src/components/inputs/checkbox/checkbox.js
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./checkbox.css";
-
+import CustomTooltip from "../../tooltip/CustomTooltip";
+import tooltip from "../../../assets/img/tooltip.svg";
 const Checkbox = ({
 	required,
 	onChange,
@@ -13,6 +14,7 @@ const Checkbox = ({
 	id,
 	fieldName,
 	index,
+	tooltipData,
 	...otherProps
 }) => {
 	//add on change
@@ -33,31 +35,22 @@ const Checkbox = ({
 				<label className="form-check-label" htmlFor={id}>
 					{fieldName}
 				</label>
-				<span
-					class="cardTooltipSvg"
-					data-toggle="popover"
-					title=""
-					data-content="In a Zero Depreciation or Nil Depreciation or Bumper to Bumper​ addon, the entire claim amount is paid by the Car Insurance Company without considering the depreciation on the value of the car. You will receive the entire cost of the car parts from the insurer excluding consumables such as Lubricants, Retainers, Brackets, Gaskets, Accessories and Wear &amp; Tear parts"
-					data-original-title="Zero Depreciation"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="13"
-						height="13"
-						viewBox="0 0 13 13"
+				<span style={{ marginLeft: "3px" }}>
+					<CustomTooltip
+						rider="true"
+						id={`${name}[${index}]tooltip`}
+						place={"bottom"}
+						customClassName="mt-3 riderPageTooltip "
 					>
-						<g transform="translate(-.5 -.5)">
-							<g transform="translate(1 1)">
-								<circle cx="6" cy="6" r="6" fill="none" stroke="#333"></circle>
-								<path
-									fill="#333"
-									d="M7.359 8.409h1.025v-.06c0-1.359 1.615-.983 1.615-2.6A1.79 1.79 0 0 0 8.025 4 2 2 0 0 0 6 5.265l1.06.453a.951.951 0 0 1 .914-.684.749.749 0 0 1 .826.726c0 .966-1.444.649-1.444 2.589zm.513 2.354a.752.752 0 1 0-.752-.752.72.72 0 0 0 .751.752z"
-									data-name="?-copy"
-									transform="translate(-2.056 -1.37)"
-								></path>
-							</g>
-						</g>
-					</svg>
+						<img
+							data-tip={`<h3 >${fieldName}</h3> <div>${tooltipData}</div>`}
+							data-html={true}
+							data-for={`${name}[${index}]tooltip`}
+							src={tooltip}
+							alt="tooltip"
+							className="toolTipRiderChild"
+						/>
+					</CustomTooltip>
 				</span>
 			</div>
 		</>
