9610aa4c661c4dda991e86d2643ef56b1d333597
AbhiS66 <Abhi668412@gmail.com>
2021-06-07 11:31:29 +0530

    wip

--- src/modules/quotesPage/quotesPage.js
+++ src/modules/quotesPage/quotesPage.js
@@ -20,7 +20,7 @@ export const QuotesPage = () => {
 		<>
 			<MainContainer>
 				<Row>
-					<FilterContainer></FilterContainer>
+					<FilterContainer />
 				</Row>
 				<Row>
 					<Col lg={3}>
@@ -82,7 +82,7 @@ export const QuotesPage = () => {
 					<Col lg={3} md={12}>
 						<AddOnsCard />
 					</Col>
-					<Col lg={9} lg={9} md={12}>
+					<Col lg={9} md={12}>
 						<Row>
 							{tab === "tab1" && (
 								<>
